d57eee7cb4cbaf23125dcd294ad34cd573df10fb
jerikejoseph <jerikejoseph144@gmail.com>
2024-10-13 17:25:41 +0530

    asd

--- request.Handler.js
+++ request.Handler.js
@@ -3,28 +3,90 @@ import userSchema from "./models/user.model.js"
 export async function addUsers(req,res){
 
 
-try {
-    console.log(req.body);
-const{name,phone,email,Address1,Address2,pin,password,cpassword}=req.body
-console.log(name,phone,email,Address1,Address2,pin,password,cpassword);
-
-
-//validation
-
-if(!(name&&phone&&email&&Address1&&Address2&&pin&&password&&cpassword)){
-   return res.status(404).status({msg:"Fields are empty"})
-     
-}
-
-let checkph=await userSchema.findOne({phone})
-  console.log(checkph)
-
-
-} catch (error) {
-    return res.status(404).status({msg:error})
+  try {
+    // console.log(req.body);
+    const{name,phone,email,Address1,Address2,pin,password,cpassword}=req.body        
     
+
+    // Validation
+     if(!(name&&phone&&email&&Address1&&Address2&&pin&&password&&cpassword))
+         return res.status(404).send({msg:"Fields Are Empty"})
+            
+        userSchema.create({name,phone,email,Address1,Address2,pin,password,cpassword}).then((data)=>{
+            return res.status(201).send({msg:data})            
+        }).catch((error)=>{
+          return  res.status(404).send({msg:error})            
+
+        })
+
+    
+} catch (error) {
+  return  res.status(404).send({msg:error})                    
+
+    }
+
 }
 
-   
+
+export async function getUser(req,res){
+  try {
+      const users=await userSchema.find();
+      console.log(users);
+      res.status(200).send({users})
+      
+  } catch (error) {
+      res.status(404).send({msg:error})
+      
+  }
 
 }
+
+// get users
+
+export async function getUsers(req,res){
+  try {
+      console.log(req.params);
+      const _id=req.params;
+      const data=await userSchema.findOne({_id})
+      res.status(200).send(data);
+      
+  } catch (error) {
+      res.status(404).send(error);
+      
+  }
+}
+
+
+
+// Update data
+
+//export async function updateUser(req,res) {
+//  try {
+//      // console.log(req.params);
+//      // console.log(req.body);
+//      const _id=req.params;
+//      const {name,phone,email,Address1,Address2,pin,password,cpassword}=req.body
+//      if(!(name&&phone&&email&&Address1&&Address2&&pin&&password&&cpassword)){
+//          return res.status(404).send({msg:"Fields Are Empty"})
+//      }
+//      
+//      userSchema.updateOne({_id},{$set:{name,phone,email,Address1,Address2,pin,password,cpassword}}).then(()=>{  
+//          console.log(req.body);
+//                    
+//          res.status(201).send({msg:"Successfully Updated"})
+//
+//      }).catch((error)=>{
+//          res.status(404).send(error)
+//      })
+//      
+//      
+//      
+//      
+//  } catch (error) {
+//      console.log(error);
+//      
+//
+//      
+//  }
+//  
+//}
--- router.js
+++ router.js
@@ -3,7 +3,10 @@ import * as rh from "./request.Handler.js"
 
 
 const router=Router()
-router.route("/addusers").post(rh.addUsers)
+router.route("/adduser").post(rh.addUsers)
+router.route("/getuser").get(rh.getUser)
+router.route("/getusers/:_id").get(rh.getUsers)
+//router.route("/updateuser/:_id").put(rh.updateUsers)
 
 
 
